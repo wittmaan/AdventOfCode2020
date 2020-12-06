1f0abbfacf19fa4c86271942c27d7ab6934ad67d
wittmaan <wittmaan@gmx.de>
2020-12-06 10:52:40 +0100

    solution day 6

--- src/main/python/day06.py
+++ src/main/python/day06.py
@@ -1,5 +1,7 @@
 import fileinput
 from collections import Counter
+from functools import reduce
+from operator import and_, or_
 
 # --- Day 6: Custom Customs ---
 # --- Part one ---
@@ -9,15 +11,11 @@ abcy
 abcz"""
 
 
-def get_answers(dat: str):
+def get_answers(dat: str, function=or_):
     splitted = dat.split("\n")
     counter_list = [Counter(_) for _ in splitted]
 
-    counter_total = Counter()
-    for counter in counter_list:
-        counter_total += counter
-
-    return len(counter_total)
+    return len(reduce(function, counter_list))
 
 
 assert get_answers(sample_group) == 6
@@ -45,8 +43,8 @@ a
 b"""
 
 
-def count_yes_answers(dat: str):
-    return sum([get_answers(_) for _ in dat.split("\n\n")])
+def count_yes_answers(dat: str, function=or_):
+    return sum([get_answers(_, function=function) for _ in dat.split("\n\n")])
 
 
 assert count_yes_answers(sample_input) == 11
@@ -60,6 +58,14 @@ assert solution_part1 == 6612
 
 # --- Part two ---
 
+assert get_answers(sample_group, function=and_) == 3
+assert get_answers("abc", function=and_) == 3
+assert get_answers("a\nb\nc", function=and_) == 0
+assert get_answers("ab\nac", function=and_) == 1
+assert get_answers("a\na\na\na", function=and_) == 1
+assert get_answers("b", function=and_) == 1
 
-# assert solution_part2 == 2224913600
-# print(f"solution part2: {solution_part2}")
+
+solution_part2 = count_yes_answers(day6_input, function=and_)
+print(f"solution part2: {solution_part2}")
+assert solution_part2 == 3268
