2bb8650a9beef8d33854f2c238643db8a2c1a02a
wittmaan <wittmaan@gmx.de>
2020-12-17 21:24:52 +0100

    solution day 11

---
+++ src/main/python/day11.py
@@ -0,0 +1,114 @@
+import fileinput
+from collections import defaultdict
+from copy import deepcopy
+from dataclasses import dataclass
+
+from typing import List
+
+# --- Day 11: Seating System ---
+# --- Part one ---
+
+sample_input = """L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL""".split(
+    "\n"
+)
+
+sample_input = [list(_.strip()) for _ in sample_input]
+
+print(sample_input)
+
+# @dataclass
+# class Position:
+
+EMPTY_SEAT = "L"
+OCCUPIED_SEAT = "#"
+FLOOR = "."
+
+
+def detect_stable_state(grid: List[str]):
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    step(grid, num_rows, num_cols)
+
+
+def count_occupied_seats(grid: List[str], idx_row: int, idx_col: int, num_rows: int, num_cols: int) -> int:
+    count = 0
+    for delta_row in [-1, 0, 1]:
+        for delta_col in [-1, 0, 1]:
+            if delta_row or delta_col:
+                new_row = idx_row + delta_row
+                new_col = idx_col + delta_col
+
+                while 0 <= new_row < num_cols and 0 <= new_col < num_rows:
+                    if grid[new_row][new_col] == "#":
+                        count += 1
+
+                    new_row += 1
+                    new_col += 1
+
+    return count
+
+
+def get_new_value(grid: List[str], idx_row: int, idx_col: int, num_rows: int, num_cols: int):
+    actual_value = grid[idx_row][idx_col]
+
+    count = count_occupied_seats(grid, idx_row, idx_col, num_rows, num_cols)
+    print(f"count={count}")
+
+    # if actual_value == EMPTY_SEAT:
+
+    return "2"
+
+
+def step(grid: List[str], num_rows: int, num_cols: int):
+    grid_copy = deepcopy(grid)
+
+    for idx_row, val_row in enumerate(grid):
+        # print("new_row")
+        for idx_col, val_col in enumerate(val_row):
+            # print(val_col)
+            grid_copy[idx_row][idx_col] = get_new_value(grid, idx_row, idx_col, num_rows, num_cols)
+
+
+detect_stable_state(sample_input)
+
+
+# sample_input1 = [int(_) for _ in sample_input1]
+
+
+# day10_input = [int(_.strip()) for _ in fileinput.input()]
+# solution_part1 = calc_differences(dat=day10_input)
+# print(f"solution part1: {solution_part1}")
+# assert solution_part1 == 1984
+
+
+# --- Part two ---
+
+
+# def detect_arrangements(dat: List[int]) -> int:
+#     dat.append(max(dat) + 3)
+#     dat.sort()
+#
+#     count_dict = defaultdict(int)
+#     count_dict[0] = 1
+#
+#     for d in dat[1:]:
+#         count_dict[d] = count_dict[d - 3] + count_dict[d - 2] + count_dict[d - 1]
+#
+#     return max(count_dict.values())
+#
+#
+# assert detect_arrangements(sample_input1) == 8
+#
+# solution_part2 = detect_arrangements(day10_input)
+# print(f"solution part2: {solution_part2}")
+# assert solution_part2 == 3543369523456
