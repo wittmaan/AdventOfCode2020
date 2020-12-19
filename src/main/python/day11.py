8b5af4a0889c141c081379e0391252d129e063d3
wittmaan <wittmaan@gmx.de>
2020-12-19 12:47:58 +0100

    solution day 11

--- src/main/python/day11.py
+++ src/main/python/day11.py
@@ -33,15 +33,18 @@ FLOOR = 2
 
 
 POSITIONS_MAPPING = {EMPTY_SEAT_ORIG: EMPTY_SEAT, OCCUPIED_SEAT_ORIG: OCCUPIED_SEAT, FLOOR_ORIG: FLOOR}
+POSITIONS_MAPPING_INVERTED = {v: k for k, v in POSITIONS_MAPPING.items()}
 
 
 class StableStateDetector:
-    def __init__(self, grid_input: List[str]):
+    def __init__(self, grid_input: List[str], visible_occupied_seats_limit: int = 4, find_first_mode: bool = False):
         self.grid = np.array(
             [[POSITIONS_MAPPING[act_position] for act_position in list(_.strip())] for _ in grid_input]
         )
         self.num_rows = len(self.grid)
         self.num_cols = len(self.grid[0])
+        self.visible_occupied_seats_limit = visible_occupied_seats_limit
+        self.find_first_mode = find_first_mode
         self.num_occupied_seats = self.run()
 
     def run(self):
@@ -63,37 +66,76 @@ class StableStateDetector:
 
         for idx_row, val_row in enumerate(grid_copy):
             for idx_col, val_col in enumerate(val_row):
-                grid_copy[idx_row][idx_col] = get_new_value(self.grid, idx_row, idx_col, self.num_rows, self.num_cols)
+                grid_copy[idx_row][idx_col] = get_new_value(
+                    self.grid,
+                    idx_row,
+                    idx_col,
+                    self.num_rows,
+                    self.num_cols,
+                    self.visible_occupied_seats_limit,
+                    self.find_first_mode,
+                )
         return grid_copy
 
     def show_grid(self):
-        print("\n".join(["".join([str(col) for col in row]) for row in self.grid]))
+
+        print("\n".join(["".join([POSITIONS_MAPPING_INVERTED[col] for col in row]) for row in self.grid]))
         print("-------------------------------")
 
 
 @njit
-def count_occupied_neighbor_seats(grid: np.ndarray, idx_row: int, idx_col: int, num_rows: int, num_cols: int) -> int:
+def find_first_seat(
+    grid: np.ndarray, idx_row: int, idx_col: int, delta_row: int, delta_col: int, num_rows: int, num_cols: int
+):
+    while True:
+        idx_row += delta_row
+        idx_col += delta_col
+
+        if 0 <= idx_row < num_rows and 0 <= idx_col < num_cols:
+            actual_seat = grid[idx_row][idx_col]
+            if actual_seat == OCCUPIED_SEAT or actual_seat == EMPTY_SEAT:
+                return actual_seat
+
+        else:
+            return EMPTY_SEAT
+
+
+@njit
+def count_occupied_neighbor_seats(
+    grid: np.ndarray, idx_row: int, idx_col: int, num_rows: int, num_cols: int, find_first_mode: bool
+) -> int:
     count = 0
     for delta_row in [-1, 0, 1]:
         for delta_col in [-1, 0, 1]:
             if delta_row or delta_col:
                 new_row = idx_row + delta_row
                 new_col = idx_col + delta_col
-                if 0 <= new_row < num_rows and 0 <= new_col < num_cols and grid[new_row][new_col] == OCCUPIED_SEAT:
-                    count += 1
+
+                if find_first_mode:
+                    count += find_first_seat(grid, idx_row, idx_col, delta_row, delta_col, num_rows, num_cols)
+                else:
+                    if 0 <= new_row < num_rows and 0 <= new_col < num_cols and grid[new_row][new_col] == OCCUPIED_SEAT:
+                        count += 1
 
     return count
 
 
 @njit
-def get_new_value(grid: np.ndarray, idx_row: int, idx_col: int, num_rows: int, num_cols: int):
+def get_new_value(
+    grid: np.ndarray,
+    idx_row: int,
+    idx_col: int,
+    num_rows: int,
+    num_cols: int,
+    visible_occupied_seats_limit: int,
+    find_first_mode: bool,
+):
     actual_value = grid[idx_row][idx_col]
-
-    count = count_occupied_neighbor_seats(grid, idx_row, idx_col, num_rows, num_cols)
+    count = count_occupied_neighbor_seats(grid, idx_row, idx_col, num_rows, num_cols, find_first_mode)
 
     if actual_value == EMPTY_SEAT and count == 0:
         return OCCUPIED_SEAT
-    elif actual_value == OCCUPIED_SEAT and count >= 4:
+    elif actual_value == OCCUPIED_SEAT and count >= visible_occupied_seats_limit:
         return EMPTY_SEAT
     else:
         return actual_value
@@ -109,22 +151,15 @@ assert solution_part1 == 2418
 
 # --- Part two ---
 
+assert (
+    StableStateDetector(
+        grid_input=sample_input, visible_occupied_seats_limit=5, find_first_mode=True
+    ).num_occupied_seats
+    == 26
+)
 
-# def detect_arrangements(dat: List[int]) -> int:
-#     dat.append(max(dat) + 3)
-#     dat.sort()
-#
-#     count_dict = defaultdict(int)
-#     count_dict[0] = 1
-#
-#     for d in dat[1:]:
-#         count_dict[d] = count_dict[d - 3] + count_dict[d - 2] + count_dict[d - 1]
-#
-#     return max(count_dict.values())
-#
-#
-# assert detect_arrangements(sample_input1) == 8
-#
-# solution_part2 = detect_arrangements(day10_input)
-# print(f"solution part2: {solution_part2}")
-# assert solution_part2 == 3543369523456
+solution_part2 = StableStateDetector(
+    grid_input=puzzle_input, visible_occupied_seats_limit=5, find_first_mode=True
+).num_occupied_seats
+print(f"solution part2: {solution_part2}")
+assert solution_part2 == 2144
