215f3164c7ecc485838cfb2442b21344405ea83c
wittmaan <wittmaan@gmx.de>
2020-12-25 17:24:31 +0100

    solution day 13

--- src/main/python/day13.py
+++ src/main/python/day13.py
@@ -1,4 +1,5 @@
 import fileinput
+from functools import reduce
 from typing import List
 
 # --- Day 13: Shuttle Search ---
@@ -13,7 +14,7 @@ sample_input = """939
 def find_earliest_bus(dat: List[str]) -> int:
     earliest_timestamp_departure = int(dat[0])
     bus_ids = [int(_) for _ in dat[1].split(",") if _ != "x"]
-    print(f"earliest_timestamp_departure {earliest_timestamp_departure} bus_ids {bus_ids}")
+    # print(f"earliest_timestamp_departure {earliest_timestamp_departure} bus_ids {bus_ids}")
 
     minimal_minutes = 100000
     minimal_bus_id = None
@@ -28,7 +29,7 @@ def find_earliest_bus(dat: List[str]) -> int:
                 minimal_minutes = diff
                 minimal_bus_id = bus_id
 
-    print(f"minimal_minutes {minimal_minutes} minimal_bus_is {minimal_bus_id}")
+    # print(f"minimal_minutes {minimal_minutes} minimal_bus_id {minimal_bus_id}")
     return minimal_minutes * minimal_bus_id
 
 
@@ -42,53 +43,57 @@ assert solution_part1 == 104
 
 # --- Part two ---
 
-# @dataclass
+# we need
+# (t + 0) % 7 == 0
+# (t + 1) % 13 == 0
+# (t + 4) % 59 == 0
+# (t + 6) % 31 == 0
+# (t + 7) % 19 == 0
+#
+# therefore
+# t % 7 == (7 - 0) % 7     -> t % 7 == 0
+# t % 13 == (13 - 1) % 13  -> t % 13 == 12
+# t % 59 == (59 - 4) % 59  -> t % 59 == 55
+# t % 31 == (31 - 6) % 36  -> t % 31 == 25
+# t % 19 == (19 - 7) % 19  -> t % 19 == 12
+
+# from https://rosettacode.org/wiki/Chinese_remainder_theorem#Python_3.6
+
+
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a * b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1:
+        return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += b0
+    return x1
 
 
 def find_matching_order(dat: List[str]) -> int:
-    bus_id_departures = [(int(val), idx) for idx, val in enumerate(dat[1].split(",")) if val != "x"]
-    print(bus_id_departures)
+    mods, remainders = zip(
+        *[(int(val), int(val) - idx % int(val)) for idx, val in enumerate(dat[1].split(",")) if val != "x"]
+    )
+    # print(f"mods={mods}, remainders={remainders}")
 
-    target_order = [_[0] for _ in bus_id_departures]
-    print(target_order)
-
-    actual_timestamps = dict.fromkeys(target_order, 0)
-    print(actual_timestamps)
-
-    for val in range(1000000):
-
-        for bus_id in target_order:
-            if val % bus_id == 0:
-                actual_timestamps[bus_id] += bus_id
-
-        tmp = [val % _ == 0 for _ in target_order]
-
-        tmp2 = sorted(actual_timestamps.items(), key=lambda item: item[1])
-
-        is_target_order_match = [_[0] for _ in tmp2] == target_order
-
-        if val > 0 and tmp[0] is True and tmp[-1] is True and is_target_order_match:
-            break
-
-    print(tmp)
-    print(val)
-    print(actual_timestamps)
-
-    # for _ in range(10):
-    #     for bus_id, departure_minutes in bus_id_departures:
-    #         actual_timestamps[bus_id] += departure_minutes
-    #
-    #     tmp = sorted(actual_timestamps.items(), key=lambda item: item[1])
-    #     print(tmp)
-    #
-    # print(actual_timestamps)
-
-    return 0
+    return chinese_remainder(n=mods, a=remainders)
 
 
-find_matching_order(sample_input)
+assert find_matching_order(sample_input) == 1068781
 
-
-# solution_part2 = detect_final_position_with_waypoint(build_instructions(puzzle_input))
-# print(f"solution part2: {solution_part2}")
-# assert solution_part2 == 24769
+solution_part2 = find_matching_order(puzzle_input)
+print(f"solution part2: {solution_part2}")
+assert solution_part2 == 842186186521918
