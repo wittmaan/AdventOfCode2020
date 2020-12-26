3d6464d2885ad1aa1907c25533f3693fdc39a1ac
wittmaan <wittmaan@gmx.de>
2020-12-26 17:24:39 +0100

    solution day 14

--- src/main/python/day14.py
+++ src/main/python/day14.py
@@ -1,4 +1,6 @@
 import fileinput
+from copy import deepcopy
+from itertools import product
 from typing import List
 
 # --- Day 14: Docking Data ---
@@ -21,36 +23,43 @@ def program(dat: List[str], floating_mode: bool = False) -> int:
             mask = value
         else:
             memory_idx = int(prefix[4:-1])
-
             if floating_mode:
                 binary_memory_idx = list(str(bin(int(memory_idx)))[2:].zfill(36))
-                print("".join(binary_memory_idx))
+                update_mask_to_one(binary_memory_idx, mask)
+                floating_idx_list = [idx for idx, val in enumerate(mask) if val == "X"]
 
-                for idx, val in enumerate(mask):
-                    if val == "1":
-                        binary_memory_idx[idx] = val
+                for floating_bit_combinations in product("01", repeat=len(floating_idx_list)):
+                    binary_memory_idx_copy = deepcopy(binary_memory_idx)
 
-                print("".join(binary_memory_idx))
-                # todo: detect floating
-                floating_idx = [idx for idx, val in enumerate(mask) if val == "X"]
-                print(floating_idx)
+                    for idx, val in enumerate(floating_idx_list):
+                        binary_memory_idx_copy[val] = floating_bit_combinations[idx]
 
+                    memory[int("".join(binary_memory_idx_copy), 2)] = int(value)
             else:
                 binary_value = list(str(bin(int(value)))[2:].zfill(36))
-                for idx, val in enumerate(mask):
-                    if val != "X":
-                        binary_value[idx] = val
-
+                update_mask_not_x(binary_value, mask)
                 memory[memory_idx] = int("".join(binary_value), 2)
 
     return sum(memory.values())
+
+
+def update_mask_not_x(binary_value, mask):
+    for idx, val in enumerate(mask):
+        if val != "X":
+            binary_value[idx] = val
+
+
+def update_mask_to_one(binary_memory_idx, mask):
+    for idx, val in enumerate(mask):
+        if val == "1":
+            binary_memory_idx[idx] = val
 
 
 assert program(sample_input) == 165
 
 puzzle_input = [_.strip() for _ in fileinput.input()]
 solution_part1 = program(puzzle_input)
-# print(f"solution part1: {solution_part1}")
+print(f"solution part1: {solution_part1}")
 assert solution_part1 == 10452688630537
 
 
@@ -63,10 +72,7 @@ mem[26] = 1""".split(
     "\n"
 )
 
-# assert program(sample_input, floating_mode=True) == 165
-print(program(sample_input2, floating_mode=True))
-
-
-# solution_part2 = find_matching_order(puzzle_input)
-# print(f"solution part2: {solution_part2}")
-# assert solution_part2 == 842186186521918
+assert program(sample_input2, floating_mode=True) == 208
+solution_part2 = program(puzzle_input, floating_mode=True)
+print(f"solution part2: {solution_part2}")
+assert solution_part2 == 2881082759597
