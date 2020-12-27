1ea0d18ecd89a3f1f68e40c6887cb1a301ef6a3b
wittmaan <wittmaan@gmx.de>
2020-12-27 20:51:27 +0100

    solution day 16

--- src/main/python/day16.py
+++ src/main/python/day16.py
@@ -27,8 +27,10 @@ nearby tickets:
 
 class Field:
     def __init__(self, name, input_str):
-        self.name = name
+        self.name: str = name
         self.values: List[int] = Field.create_list(input_str)
+        self.position = None
+        self.ticket = None
 
     @staticmethod
     def create_list(input_str: str) -> List[int]:
@@ -40,23 +42,43 @@ class Field:
 
         return result
 
+    def __lt__(self, other):
+        return self.position < other.position
+
 
 class TicketTranslation:
     def __init__(self, dat: List[str]):
         self.fields = []
         self.nearby_tickets = []
         self.your_tickets = None
+        self.invalid_values = []
         self.fill(dat)
 
     def get_error_rate(self) -> int:
-        # print(f"nearby_tickets {nearby_tickets}")
-
         invalid_nearby_tickets = []
+        valid_nearby_tickets = []
         for nearby_ticket in self.nearby_tickets:
-            if not any([nearby_ticket in field.values for field in self.fields]):
+            if not self.is_valid_ticket(nearby_ticket):
                 invalid_nearby_tickets.append(nearby_ticket)
+            else:
+                valid_nearby_tickets.append(nearby_ticket)
 
-        return sum(invalid_nearby_tickets)
+        self.nearby_tickets = valid_nearby_tickets
+        return sum(self.invalid_values)
+
+    def is_valid_ticket(self, ticket):
+        is_valid = []
+        for t in ticket:
+            checks = []
+            for field in self.fields:
+                checks.append(t in field.values)
+
+            if not any(checks):
+                self.invalid_values.append(t)
+
+            is_valid.append(any(checks))
+
+        return all(is_valid)
 
     def fill(self, dat: List[str]):
         mode = FIELD_RANGES
@@ -76,14 +98,54 @@ class TicketTranslation:
                 self.fields.append(Field(field_name, value_range))
             elif mode == YOUR_TICKET:
                 self.your_tickets = [int(_) for _ in d.split(",")]
-                # print(f"your_tickets {your_tickets}")
             elif mode == NEARBY_TICKETS:
                 sub_nearby_tickets = [int(_) for _ in d.split(",")]
-                self.nearby_tickets.extend(sub_nearby_tickets)
+                self.nearby_tickets.append(sub_nearby_tickets)
+
+    def get_departure_values(self):
+        TicketTranslation.assign_positions(self.nearby_tickets, self.fields)
+        self.update_fields()
+        TicketTranslation.assign_positions(self.your_tickets, self.fields)
+
+        result = 1
+        for field in self.fields:
+            if field.name.startswith("departure"):
+                result *= field.ticket
+        return result
+
+    @staticmethod
+    def assign_positions(tickets, fields):
+        position = 1
+        for ticket in tickets:
+            for field in fields:
+                if all(t in field.values for t in ticket) and not field.position:
+                    field.position = position
+                    field.ticket = ticket
+                    position += 1
+                    break
 
 
-tt = TicketTranslation(sample_input)
-assert tt.get_error_rate() == 71
+    def update_fields(self):
+        # check remaining fields
+        position = -1
+        for field in self.fields:
+            if field.position and field.position > position:
+                position = field.position
+
+        for field in self.fields:
+            if not field.position:
+                position += 1
+                field.position = position
+
+        self.fields.sort()
+        for field in self.fields:
+            field.position = None
+            field.ticket = None
+
+
+tt_sample = TicketTranslation(sample_input)
+assert tt_sample.get_error_rate() == 71
+assert tt_sample.nearby_tickets == [[7, 3, 47]]
 
 puzzle_input = [_.strip() for _ in fileinput.input()]
 tt = TicketTranslation(puzzle_input)
@@ -94,8 +156,25 @@ assert solution_part1 == 25895
 
 # --- Part two ---
 
-# assert get_number_spoken(sample_input, at_turn=30000000) == 175594
-#
-# solution_part2 = get_number_spoken("7,12,1,0,16,2", at_turn=30000000)
-# print(f"solution part2: {solution_part2}")
-# assert solution_part2 == 238
+sample_input2 = """class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9""".split(
+    "\n"
+)
+
+tt_sample2 = TicketTranslation(sample_input2)
+assert tt_sample2.get_error_rate() == 0
+assert tt_sample2.invalid_values == []
+assert tt_sample2.get_departure_values() == 1
+
+#solution_part2 = tt.get_departure_values()
+#print(f"solution part2: {solution_part2}")
+# assert solution_part1 == 25895
